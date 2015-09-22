ef8b2b68688d7530b62dc273837bda5cd0dce217
jpggvilaca <jpggvilaca@gmail.com>
2015-09-22 22:46:02 +0100

    created directory tree; add a couple of gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var webserver = require('gulp-webserver');
+var autoprefixer = require('gulp-autoprefixer');
+var coffee = require('gulp-coffee');
+var sass = require('gulp-sass');
+
 
 gulp.task('default', function() {
-  // place code for your default task here
+  return gulp.src('src/app.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('webserver', function() {
@@ -13,4 +23,20 @@ gulp.task('webserver', function() {
       open: false,
       port: 3500
     }));
+});
+
+gulp.task('coffee', function() {
+  gulp.src('./src/*.coffee')
+    .pipe(coffee({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
