4af386e143e0f87001198ce0190f01f2c01ffbaf
jpggvilaca <jpggvilaca@gmail.com>
2019-03-24 22:29:15 +0100

    Update folder structure pt II

---
+++ build/precache-manifest.7bef18fa702ea794a252f4e22914e878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e3defdeac79ac3e7ad7",
+    "url": "/static/css/main.d6bdb770.chunk.css"
+  },
+  {
+    "revision": "3e3defdeac79ac3e7ad7",
+    "url": "/static/js/main.1c6582a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "44461e975102ef37129b",
+    "url": "/static/js/2.716bab56.chunk.js"
+  },
+  {
+    "revision": "d14730b3ffd95aea05b020efda3d3b6c",
+    "url": "/static/media/avatar.d14730b3.jpg"
+  },
+  {
+    "revision": "0544b633eaaba4002b58997cb5f01e12",
+    "url": "/index.html"
+  }
+];
