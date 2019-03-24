b4fd8e87c0ab36ec61ebdbce980bdd24f8d1863f
jpggvilaca <jpggvilaca@gmail.com>
2019-03-24 22:51:19 +0100

    Updates

---
+++ precache-manifest.af14dcace27055c3516320a677680e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4a5e66cfce89c005860",
+    "url": "/me/static/css/main.7d6821da.chunk.css"
+  },
+  {
+    "revision": "a4a5e66cfce89c005860",
+    "url": "/me/static/js/main.1c6582a9.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "44461e975102ef37129b",
+    "url": "/me/static/js/2.716bab56.chunk.js"
+  },
+  {
+    "revision": "d14730b3ffd95aea05b020efda3d3b6c",
+    "url": "/me/static/media/avatar.d14730b3.jpg"
+  },
+  {
+    "revision": "d1df4a7bd9e429da5c913869bc39b7d5",
+    "url": "/me/index.html"
+  }
+];
